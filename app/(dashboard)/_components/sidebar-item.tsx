7c5af4591b122f0ccc41fba26875ab9460885e2a
Alex Okeyo <alexokeyo15@gmail.com>
2024-07-12 16:48:51 +0300

    update

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -1,6 +1,27 @@
 "use-client";
 
-export const SidebarItem = () => {
+import { LucideIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
+
+interface SidebarItemProps{
+    icon: LucideIcon;
+    label: string;
+    href: string;
+}
+
+export const SidebarItem = ({
+    icon: Icon,
+    label,
+    href,
+}: SidebarItemProps) => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const isActive = 
+    (pathname === "/" && href === "/") ||
+    pathname === href ||
+    pathname?.startsWith('${href}/');
     return (
         <div>
             Sidebar Item!
