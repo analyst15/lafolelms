2b6ce693918869418db1200232f7585200ade221
Alex Okeyo <alexokeyo15@gmail.com>
2024-07-13 13:04:44 +0300

    sidebar

--- app/(dashboard)/_components/sidebar-item.tsx
+++ app/(dashboard)/_components/sidebar-item.tsx
@@ -3,8 +3,8 @@
 import { cn } from "@/lib/utils";
 import { LucideIcon } from "lucide-react";
 import { usePathname } from "next/navigation";
-import { useRouter } from "next/router";
-import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+
 
 interface SidebarItemProps{
     icon: LucideIcon;
@@ -32,8 +32,22 @@ export const SidebarItem = ({
         <button
         onClick={onClick}
         type="button"
-        className={cn}
+        className={cn(
+            "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+            isActive && "text-sky-700 bg-sky-200 hover:bg-sky-200/20 hover:text-sky-700"
+        )}
         >
+        
+        <div className="flex items-center gap-x-2 py-4">
+            <Icon 
+            size={22}
+            className={cn(
+                "text-slate-700",
+                isActive && "text-sky-700"
+            )}
+            />
+            {label}
+        </div>
 
         </button>
     )
