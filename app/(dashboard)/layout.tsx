d40e5861ad162617950a9fbda6b8f2eed294983d
Alex Okeyo <alexokeyo15@gmail.com>
2024-07-15 14:47:54 +0300

    mobile navbar toggler

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import { Sidebar } from './_components/sidebar';
+import { Navbar } from './_components/navbar';
 const DashboardLayout = ({
     children
 }: {
@@ -6,6 +7,9 @@ const DashboardLayout = ({
 }) => {
     return (
         <div className="h-full">
+            <div className="h-[80px] md:pl-50 fixed inset-y-0 w-full z-50">
+                <Navbar />
+            </div>
             <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
