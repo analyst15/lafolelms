554c352505530861b1180c56d32834033f483645
Alex Okeyo <alexokeyo15@gmail.com>
2024-07-15 14:34:43 +0300

    search page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,10 +6,12 @@ const DashboardLayout = ({
 }) => {
     return (
         <div className="h-full">
-            <div className="hidden md:flex h-full w-56 flex-col insert-y-0 z-50">
+            <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
                 <Sidebar />
             </div>
+            <main className='md:pl-56 h-full'>
             {children}
+            </main>
         </div>
     );
 }
