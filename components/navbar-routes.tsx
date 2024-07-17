a9d93918e7412fc1eab07ef3639acf99940c23da
Alex Okeyo <alexokeyo15@gmail.com>
2024-07-17 15:50:48 +0300

    exit button

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,26 +1,28 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { LogOut } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import Link  from "next/link";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
     const pathname = usePathname();
-    const router = useRouter();
+
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayer = pathname?.includes("/chapter")
 
-    return(
+    return (
         <div className="flex gap-x-2 ml-auto ">
             {isTeacherPage || isPlayer ? (
-                <Button size="sm" variant="ghost">
-                    <LogOut className="h-4 w-4 mr-2" />
-                    Exit
-                </Button>
+                <Link href="/">
+                    <Button size="sm" variant="ghost">
+                        <LogOut className="h-4 w-4 mr-2" />
+                        Exit
+                    </Button>
+                </Link>
             ) : (
                 <Link href="/teacher/courses">
                     <Button size="sm" variant="ghost">
@@ -29,7 +31,7 @@ export const NavbarRoutes = () => {
                 </Link>
             )}
             <UserButton
-            afterSignOutUrl="/"
+                afterSignOutUrl="/"
             />
         </div>
     )
