a94d1ae59f3b9698a898220e1c6417260842df84
Alex Okeyo <alexokeyo15@gmail.com>
2024-09-25 16:36:37 +0300

    creating an icon

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -48,7 +48,8 @@ const CourseIdPage = async ({
                         </span>
                     </div>
                 </div>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
                     <div>
                         <div className="flex items-center gap-x-2">
                             <h2 className="text-xl">
@@ -57,7 +58,6 @@ const CourseIdPage = async ({
                         </div>
                     </div>
                 </div>
-            </div>
         </div>
      );
 }
