1a7ef69758d9aaa7b1335ee2320ab5605c250fed
Alex Okeyo <alexokeyo15@gmail.com>
2024-09-26 16:26:47 +0300

    icon creation

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,7 @@
+import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db"
 import { auth } from "@clerk/nextjs/server";
+import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 const CourseIdPage = async ({
     params
@@ -52,6 +54,7 @@ const CourseIdPage = async ({
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
                     <div>
                         <div className="flex items-center gap-x-2">
+                            <IconBadge icon={LayoutDashboard}/>
                             <h2 className="text-xl">
                                 Customize your course
                             </h2>
