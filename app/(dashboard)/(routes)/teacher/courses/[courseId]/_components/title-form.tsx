2c061253f25240587d2402083e8197ab8488ae40
Alex Okeyo <alexokeyo15@gmail.com>
2024-09-27 16:59:34 +0300

    form creation

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/title-form.tsx
@@ -23,10 +23,22 @@ interface TitleFormProps {
     courseId: string
 };
 
+const formSchema = z.object({
+    title: z.string().min(1, {
+        message: "Title is required"
+    })
+});
+
 export const TitleForm = ({
     initialData,
     courseId
 }: TitleFormProps) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData,
+  })  
+
+  const{ isSubmitting, isValid } = form.formState
     return(
         <div>
         Title Form
