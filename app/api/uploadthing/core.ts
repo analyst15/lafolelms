27683ede1eb3abf2d776dc52b7c805b2dc23b6d2
Alex Okeyo <alexokeyo15@gmail.com>
2024-10-14 16:35:07 +0300

    Updates

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,14 +1,24 @@
+import { error } from "console";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { auth } from "@clerk/nextjs/server";
 import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
+
+const handleAuth = () => {
+    const { userId } = auth();
+    if (!userId) throw new Error("Unauthorized");
+    return  { userId };
+}
 
 const auth = (req: Request) => ({ id: "fakeId" }); 
 
 
 export const ourFileRouter = {
+    courseImage: f({image: { maxFileSize: "4MB", maxFileCount: 1} })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {})
 
-    
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
