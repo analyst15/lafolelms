4cc8daff8de77d7764e7a36c030a319bc860edb3
Alex Okeyo <alexokeyo15@gmail.com>
2024-10-23 13:22:10 +0300

    adding the host name "utfs.io"

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                pathname: '**', // Match any path under this domain
+            }
+        ]
+    }
+};
 export default nextConfig;
