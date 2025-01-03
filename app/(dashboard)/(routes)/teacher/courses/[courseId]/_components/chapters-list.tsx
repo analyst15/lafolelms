3a809ea4e1dc596adb813dcbe2a0f23c59c69516
Alex Okeyo <alexokeyo15@gmail.com>
2025-01-03 21:49:14 +0300

    Updates

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -1,6 +1,14 @@
 "use client"
 
 import { Chapter } from "@prisma/client";
+import { useEffect, useState } from "react";
+import {
+    DragDropContext,
+    Droppable,
+    Draggable,
+    DropResult,
+} from "@hello-pangea/dnd";
+import { cn } from "@/lib/utils"
 
 interface ChaptersListProps {
     items: Chapter[];
@@ -13,9 +21,46 @@ export const ChaptersList =({
     onReorder,
     onEdit
 }: ChaptersListProps) => {
+    const [isMounted, setIsMounted] = useState(false);
+    const [chapters, setChapters] = useState(items);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    useEffect(() => {
+        setChapters(items);
+    }, [items]);
+
+    if (!isMounted) {
+        return null;
+    }
     return (
-        <div>
-            ChaptersList
-        </div>
+        <DragDropContext onDragEnd={() => {}}>
+            <Droppable droppableId="chapters">
+                {(provided) => (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                        {chapters.map((chapter, index) => (
+                            <Draggable 
+                                key={chapter.id} 
+                                draggableId={chapter.id} 
+                                index={index}>
+                            
+                            {(provided) => (
+                                <div className={cn("flex items-center gap-x-2 bg-slate-200 border-slate-200 text-slate-700 rounded-md mb-4 text-sm",
+                                    chapter.isPublished && "bg-sky-100 border-sky-200 text-sky-700"
+                                )}
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                >
+                                </div>
+                            )}
+
+                            </Draggable>
+                        ))}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
     )
 }
