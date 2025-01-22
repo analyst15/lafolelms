7be42b16552b642c5454c0e7c223e0f4e0405443
Alex Okeyo <alexokeyo15@gmail.com>
2025-01-22 15:27:15 +0300

    adding course chapters

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-list.tsx
@@ -8,7 +8,9 @@ import {
     Draggable,
     DropResult,
 } from "@hello-pangea/dnd";
+import { Grip, Pencil } from "lucide-react";
 import { cn } from "@/lib/utils"
+import { Badge } from "@/components/ui/badge";
 
 interface ChaptersListProps {
     items: Chapter[];
@@ -32,11 +34,33 @@ export const ChaptersList =({
         setChapters(items);
     }, [items]);
 
+    const onDragEnd = (result: DropResult) => {
+        if (!result.destination) return;
+
+        const items = Array.from(chapters);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+
+        const startIndex = Math.min(result.source.index, result.destination.index);
+        const endIndex = Math.max(result.source.index, result.destination.index)
+
+        const updatedChapters = items.slice(startIndex, endIndex + 1);
+
+        setChapters(items);
+
+        const bulkUpdateData = updatedChapters.map((chapter) => ({
+            id: chapter.id,
+            position: items.findIndex((item) => item.id === chapter.id)
+        }));
+
+        onReorder(bulkUpdateData);
+    }
+
     if (!isMounted) {
         return null;
     }
     return (
-        <DragDropContext onDragEnd={() => {}}>
+        <DragDropContext onDragEnd={onDragEnd}>
             <Droppable droppableId="chapters">
                 {(provided) => (
                     <div {...provided.droppableProps} ref={provided.innerRef}>
@@ -58,15 +82,34 @@ export const ChaptersList =({
                                 className={cn("px-2 py-3 border-r border-r-slate-200 hover:bg-slate-300 rounded-l-md transition",
                                      chapter.isPublished && "border-r-sky-200 hover:bg-sky-200"
                                 )}
+                                {...provided.dragHandleProps}
                                 >
-
+                                    <Grip 
+                                    className="h-5 w-5"
+                                    />
                                 </div>
+                                {chapter.title}
+                                <div className="ml-auto pr-2 flex items-center gap-x-2">
+                                    {chapter.isFree && (
+                                        <Badge>Free</Badge>
+                                    )}
+                                    <Badge 
+                                    className={cn("bg-slate-500",chapter.isPublished && "bg-sky-700")}>
+                                    
+                                    {chapter.isPublished ? "Published" : "Draft"}
 
+                                    </Badge>
+                                    <Pencil
+                                        onClick={() => onEdit(chapter.id)}
+                                        className="w-4 h-4 cursor-pointer hover:opacity-75 transition" 
+                                    />
+                                </div>
                                 </div>
                             )}
 
                             </Draggable>
                         ))}
+                        {provided.placeholder}
                     </div>
                 )}
             </Droppable>
