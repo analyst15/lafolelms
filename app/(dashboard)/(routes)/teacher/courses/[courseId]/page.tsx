3686f378156d75434547fe687cb275ba3f52a925
Alex Okeyo <alexokeyo15@gmail.com>
2025-04-05 14:37:51 +0300

    import actions

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -10,6 +10,8 @@ import { CategoryForm } from "./_components/category-form";
 import { PriceForm } from "./_components/price-form";
 import { AttachmentForm } from "./_components/attachment-form";
 import { ChaptersForm } from "./_components/chapters-form";
+import { Banner } from "@/components/banner";
+import { Actions } from "./_components/actions";
 const CourseIdPage = async ({
     params
 }: {
@@ -64,7 +66,16 @@ const CourseIdPage = async ({
 
     const completionText = `(${completedFields}/${totalFields})`
 
-    return ( 
+    const isComplete = requiredFields.length;
+
+    
+    return (
+        <>
+        {!course.isPublished && (
+            <Banner 
+            label="This course is unpublished. It will not be visible to the students."
+            />
+        )} 
         <div className="p-6">
             <div className="flex items-center justify-between">
                 <div className="flex flex-col gap-y-2">
@@ -77,6 +88,11 @@ const CourseIdPage = async ({
                         </span>
                     </div>
                 </div>
+                <Actions 
+                    disabled={!isComplete}
+                    courseId={params.courseId}
+                    isPublished={course.isPublished}
+                    />
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
                     <div>
@@ -147,6 +163,7 @@ const CourseIdPage = async ({
                     </div>
                 </div>
         </div>
+        </>
      );
 }
  
