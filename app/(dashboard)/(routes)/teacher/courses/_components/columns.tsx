0ebbe42c8ecc029205c189de4897f365bc636cb5
Alex Okeyo <alexokeyo15@gmail.com>
2025-04-14 16:44:26 +0300

    import the dropdown menu

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -2,8 +2,16 @@
 
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown } from "lucide-react"
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import Link from "next/link"
+
 
 export const columns: ColumnDef<Course>[] = [
   {
@@ -47,5 +55,30 @@ export const columns: ColumnDef<Course>[] = [
         </Button>
       )
     },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const { id } = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-4 w-8 p-0">
+              <span className="sr-only">Open Menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <Link href={`/teacher/courses/${id}`}>
+            <DropdownMenuItem>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit
+            </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    }
   },
 ]
