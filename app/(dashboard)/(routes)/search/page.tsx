7f1b2cf0bb65fb0dde3107db9c50715b86447156
analyst15 <alexokeyo15@gmail.com>
2025-04-27 15:56:30 +0300

    search function

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,12 +1,36 @@
 import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { CoursesList } from "@/components/courses-list";
 
-const SearchPage = async () => {
+interface SearchPageProps {
+    searchParams: {
+        title: string;
+        categoryId: string;
+    }
+}
+
+const SearchPage = async ({
+    searchParams
+}: SearchPageProps) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
     const categories = await db.category.findMany({
         orderBy: {
             name: "asc"
         }
+    });
+
+    const courses = await getCourses({
+        userId,
+        ...searchParams,
     });
     return ( 
         <>
@@ -17,6 +41,7 @@ const SearchPage = async () => {
                 <Categories
                     items={categories}
                 />
+                <CoursesList items={courses} />
             </div>
         </>
      );
