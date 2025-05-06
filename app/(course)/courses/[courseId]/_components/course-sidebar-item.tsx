194e0fa2c37c34e1e77a1ac9e3b63638e50e3b63
analyst15 <alexokeyo15@gmail.com>
2025-05-06 22:49:52 +0300

    working on the sidebar

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { cn } from "@/lib/utils";
 import { CheckCircle, Lock, PlayCircle } from "lucide-react";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -30,11 +31,33 @@ export const CourseSidebarItem = ({
     }
 
     return ( 
-        <button>
-            <div>
-                <Icon />
+        <button
+        type="button"
+        className={cn(
+            "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20" ,
+            isActive && "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700",
+            isCompleted && "text-emerald-700 hover:text-emerald-700",
+            isCompleted && isActive && "bg-emerald-200/20",
+        )}
+        >
+            <div className="flex items-center gap-x-2 py-4">
+                <Icon
+                size={22}
+                className={cn(
+                    "text-slate-500",
+                    isActive && "text-slate-700",
+                    isCompleted && "text-emerald-700"
+                )}
+                />
                 {label}
             </div>
+            <div className={cn(
+                "ml-auto opacity-0 border-2 border-slate-700 h-full transition-all",
+                isActive && "opacity-100",
+                isCompleted && "border-emaerald-700"
+            )}
+            
+            />
         </button>
      );
 }
