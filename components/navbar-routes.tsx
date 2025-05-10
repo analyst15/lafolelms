8ff1e9117092139633dac0a9d761cc8dd9e3d1c5
analyst15 <alexokeyo15@gmail.com>
2025-05-10 22:34:31 +0300

    change isPlayer to isCoursePage

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -13,7 +13,7 @@ export const NavbarRoutes = () => {
 
 
     const isTeacherPage = pathname?.startsWith("/teacher");
-    const isPlayer = pathname?.includes("/chapter");
+    const isCoursePage = pathname?.includes("/courses");
     const isSearchPage = pathname === "/search";
 
     return (
@@ -24,7 +24,7 @@ export const NavbarRoutes = () => {
             </div>
         )}
             <div className="flex gap-x-2 ml-auto ">
-                {isTeacherPage || isPlayer ? (
+                {isTeacherPage || isCoursePage ? (
                     <Link href="/">
                         <Button size="sm" variant="ghost">
                             <LogOut className="h-4 w-4 mr-2" />
