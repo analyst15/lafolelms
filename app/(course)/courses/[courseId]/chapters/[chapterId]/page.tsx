98d0976567dd6af5cbbdccdb05f891bc955b8039
analyst15 <alexokeyo15@gmail.com>
2025-05-11 22:03:24 +0300

    new file page.tsx

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,23 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+    params
+}: {
+    params: { courseId: string; chapterId: string }
+}) => {
+
+    const { userId } = auth();
+
+    if(!userId) {
+        return redirect("/")
+    }
+
+    return ( 
+        <div>
+            Chapter Id!
+        </div>
+     );
+}
+ 
+export default ChapterIdPage;
