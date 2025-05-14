a496b12a8a9deeb9fff4403a8208e4e567e6ee9e
analyst15 <alexokeyo15@gmail.com>
2025-05-14 22:09:11 +0300

    course purchase logic

--- actions/get-chapter.ts
+++ actions/get-chapter.ts
@@ -21,31 +21,79 @@ export const getChapter = async ({
                 }
             }
         });
-    
-    const course = await db.course.findUnique({
-        where: {
-            isPublished: true,
-            id: courseId,
-        },
-        select: {
-            price: true,
+
+        const course = await db.course.findUnique({
+            where: {
+                isPublished: true,
+                id: courseId,
+            },
+            select: {
+                price: true,
+            }
+        });
+
+        const chapter = await db.chapter.findUnique({
+            where: {
+                id: chapterId,
+                isPublished: true,
+            }
+        })
+
+        if (!chapter || !course) {
+            throw new Error("Chapter course not found");
         }
-    });
 
-    const chapter = await db.chapter.findUnique({
-        where: {
-            id: chapterId,
-            isPublished: true,
+        let muxData = null;
+        let attachments: Attachment[] = [];
+        let nextChapter: Chapter | null = null
+
+        if (purchase) {
+            attachments = await db.attachment.findMany({
+                where: {
+                    courseId: courseId
+                }
+            });
+
+            if (chapter.isFree || purchase) {
+                muxData = await db.muxData.findUnique({
+                    where: {
+                        chapterId: chapterId,
+                    }
+                });
+
+                nextChapter = await db.chapter.findFirst({
+                    where: {
+                        courseId: courseId,
+                        isPublished: true,
+                        position: {
+                            gt: chapter?.position,
+                        }
+                    },
+                    orderBy: {
+                        position: "asc",
+                    }
+                });
+            }
         }
-    })
 
-    if (!chapter || !course) {
-        throw new Error("Chapter course not found");
-    }
+        const userProgress = await db.userProgress.findUnique({
+            where: {
+                userId_chapterId: {
+                    userId,
+                    chapterId,
+                }
+            }
+        });
 
-    let muxData = null;
-    let attachments: Attachment[] = [];
-    let nextChapter: Chapter | null = null
+        return {
+            chapter,
+            course,
+            muxData,
+            attachments,
+            nextChapter,
+            userProgress,
+            purchase,
+        };
 
     } catch (error) {
         console.log("[GET_CHAPTER]", error);
