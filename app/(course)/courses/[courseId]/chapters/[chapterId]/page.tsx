275af5a48b46aaa90c4251752ace168e03a8350c
analyst15 <alexokeyo15@gmail.com>
2025-05-14 22:09:52 +0300

    course purchase imports and logic

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,5 +1,8 @@
+import { getChapter } from "@/actions/get-chapter";
+import { Banner } from "@/components/banner";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { VideoPlayer } from "./_components/video-player";
 
 const ChapterIdPage = async ({
     params
@@ -13,9 +16,52 @@ const ChapterIdPage = async ({
         return redirect("/")
     }
 
+    const {
+        chapter,
+        course,
+        muxData,
+        attachments,
+        nextChapter,
+        userProgress,
+        purchase,
+    } = await getChapter({
+        userId,
+        chapterId: params.chapterId,
+        courseId: params.courseId,
+    })
+
+    if (!chapter || !course) {
+        return redirect("/")
+    }
+
+    const isLocked = !chapter.isFree && !purchase;
+    const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
     return ( 
         <div>
-            Chapter Id!
+            {userProgress?.isCompleted && (
+                <Banner
+                variant="success"
+                label="You already completed this chapter."
+                />
+            )}
+            {isLocked && (
+                <Banner
+                variant="warning"
+                label="You need to purchase this course to watch this chapter."
+                />
+            )}
+            <div className="flex flex-col max-w-4xl mx-auto pb-20">
+                <VideoPlayer
+                chapterId={params.chapterId}
+                title={chapter.title}
+                courseId={params.courseId}
+                nextChapterId={nextChapter?.id}
+                playbackId={muxData?.playbackId!}
+                isLocked={isLocked}
+                completeOnEnd={completeOnEnd}
+                />
+            </div>
         </div>
      );
 }
