2b1c2ad132fd222c013b5ad9694b45bf0ccda7ed
analyst15 <alexokeyo15@gmail.com>
2025-05-18 21:54:23 +0300

    mux video player

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -28,12 +28,34 @@ export const VideoPlayer = ({
     completeOnEnd,
     title,
 }: VideoPlayerProps) => {
+    const [isReady, setIsReady] = useState(false);
+    
     return (
         <div className="relative aspect-video">
-            {!isLocked && (
+            {!isReady && !isLocked && (
                 <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
                     <Loader2 className="h-8 w-8 animate-spin text-secondary" />
                 </div>
+            )}
+            {isLocked &&  (
+                <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+                    <Lock className="h-8 w-8" />
+                    <p className="text-sm">
+                        This chapter is locked
+                    </p>
+                </div>
+            )}
+            {!isLocked && (
+                <MuxPlayer
+                title={title}
+                className={cn(
+                    !isReady && "hidden"
+                )}
+                onCanPlay={() => setIsReady(true)}
+                onEnded={() => {}}
+                autoPlay
+                playbackId={playbackId}
+                />
             )}
         </div>
     )
